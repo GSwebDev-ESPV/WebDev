33563dce73911bb914b447b7ea6be538df203f94
Fabricio Denig <fabriciodenig@gmail.com>
2026-06-08 21:46:47 -0300

    Cria perguntas e alternativas do quiz

--- js/script.js
+++ js/script.js
@@ -29,3 +29,67 @@ document.getElementById("formQuiz").addEventListener("submit",function(e){
 
     alert("Bem-vindo(a), " + nome);
 });
+
+const perguntas = [
+
+{
+pergunta:"O que a AstraLink monitora?",
+opcoes:["Trânsito","Conectividade","Clima"],
+correta:1
+},
+
+{
+pergunta:"Qual tecnologia conecta áreas remotas?",
+opcoes:["Fibra","Bluetooth","Satélite"],
+correta:2
+},
+
+{
+pergunta:"Quem se beneficia da AstraLink?",
+opcoes:["Comunidades rurais","Pilotos","Turistas"],
+correta:0
+},
+
+{
+pergunta:"Qual setor é beneficiado?",
+opcoes:["Educação","Cinema","Moda"],
+correta:0
+},
+
+{
+pergunta:"A AstraLink usa:",
+opcoes:["IoT","DVD","Fax"],
+correta:0
+},
+
+{
+pergunta:"Os dados são mostrados em:",
+opcoes:["Dashboard","Calculadora","Editor"],
+correta:0
+},
+
+{
+pergunta:"Qual benefício principal?",
+opcoes:["Monitoramento","Jogos","Streaming"],
+correta:0
+},
+
+{
+pergunta:"A solução ajuda:",
+opcoes:["Gestores","Cantores","Atletas"],
+correta:0
+},
+
+{
+pergunta:"A conectividade melhora:",
+opcoes:["Serviços digitais","Propaganda","Turismo"],
+correta:0
+},
+
+{
+pergunta:"O objetivo é:",
+opcoes:["Resolver falhas","Vender celulares","Criar filmes"],
+correta:0
+}
+
+];
