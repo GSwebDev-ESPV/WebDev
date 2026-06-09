adbe703b724f5d04dd64c5001792d00a50e92178
Fabricio Denig <fabriciodenig@gmail.com>
2026-06-08 21:44:41 -0300

    Adiciona validação do formulário inicial

--- js/script.js
+++ js/script.js
@@ -15,3 +15,17 @@ setInterval(() => {
 
     document.getElementById("slide").src = imagens[slideAtual];
 },3000);
+
+document.getElementById("formQuiz").addEventListener("submit",function(e){
+
+    e.preventDefault();
+    const nome =
+    document.getElementById("nome").value;
+
+    if(nome.trim() === ""){
+        alert("Digite seu nome.");
+        return;
+    }
+
+    alert("Bem-vindo(a), " + nome);
+});
